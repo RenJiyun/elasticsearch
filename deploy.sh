9b673ce9e4be340a59c9b68fef8601805706f326
renjiyun <wozaijixu@msn.cn>
2023-11-22 08:29:50 +0800

    修改 .gitignore

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+scp /home/ren/work/code/elasticsearch/distribution/archives/linux-tar/build/distributions/elasticsearch-8.11.2-SNAPSHOT-linux-x86_64.tar.gz \
+    /home/ren/.tmp/es
+
+scp /home/ren/work/code/elasticsearch/distribution/archives/linux-tar/build/distributions/elasticsearch-8.11.2-SNAPSHOT-linux-x86_64.tar.gz \
+    ren@192.168.122.226:/home/ren/.tmp/es
+
+scp /home/ren/work/code/elasticsearch/distribution/archives/linux-tar/build/distributions/elasticsearch-8.11.2-SNAPSHOT-linux-x86_64.tar.gz \
+    ren@192.168.3.121:/home/ren/.tmp/es
